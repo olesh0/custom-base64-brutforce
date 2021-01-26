79f5dc74254fee583e52681846eba71282822cf8
olesh <olegkirashchuk@gmail.com>
2021-01-26 09:36:55 +0200

    create cipher_files directory

--- brutforce.py
+++ brutforce.py
@@ -1,3 +1,5 @@
+# TODO Brutforce all possible options.
+
 if __name__ == "__main__":
   words_to_expect_raw = input("Words to expect (divided by |): ")
   words_to_expect = words_to_expect_raw.split('|')
